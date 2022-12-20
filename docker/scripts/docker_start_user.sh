7abd03fce546ff185d07e05e78df42a67ae7fe82
Kirill Mitkovskii <kirill.mitkovskii@gmail.com>
2022-12-19 20:06:31 -0400

    Fix version plate

--- docker/scripts/docker_start_user.sh
+++ docker/scripts/docker_start_user.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ###############################################################################
-# Copyright 2020 The Innopolis University Authors. All Rights Reserved.
+# Copyright 2022 Kirill Mitkovskii. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
